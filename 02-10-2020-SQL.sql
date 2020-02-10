0a5990ad68e7f8666c3d0effa8f1b171cfe1cbbf
David Michael Wederstrandt Sr <david.wederstrandtsr@gmail.com>
2020-02-10 11:25:20 -0600

    pushing ex 3.5.1

---
+++ 02-10-2020-SQL.sql
@@ -0,0 +1,69 @@
+USE employees;
+
+# all employees with first name Irena, Vidya, Maya  using IN 
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+# all employees with first name begins with E
+SELECT *
+FROM employees
+WHERE first_name LIKE 'E%';
+
+# all employees hired in the 90'
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%';
+
+# all employees born on Christmas
+SELECT *
+FROM employees 
+WHERE birth_date LIKE '%-12-25';
+
+# all employees with at q in their last name
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+# all employees with first name Irena OR Vidya OR Maya   
+SELECT *
+FROM employees
+WHERE first_name LIKE 'Irena' OR first_name LIKE 'Vidya' OR first_name LIKE 'Maya';
+
+
+# all employees with first name Irena OR Vidya OR Maya and gender is male   
+SELECT *
+FROM employees
+WHERE (first_name LIKE 'Irena' OR first_name LIKE 'Vidya' OR first_name LIKE 'Maya') AND gender LIKE 'M';
+
+
+# all employees who's last name begins or ends with E
+SELECT *
+FROM employees 
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+# all employees who's last name begins or ends with E
+SELECT *
+FROM employees 
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+# all employees hired in the 90's and born on Christmas
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
+
+# all employees with q but not qu in the last name
+SELECT *
+FROM employees 
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
+
+
+
+
+
+
+
+
+
+
