f930efc6f4ebfbee6a6cabf61fd8548dbd08b648
David Michael Wederstrandt Sr <david.wederstrandtsr@gmail.com>
2020-02-11 13:28:42 -0600

    adding 3.7

---
+++ 3.7_group_by_exercises.sql
@@ -0,0 +1,60 @@
+# 2 select distinct titles
+SELECT DISTINCT 
+	title
+FROM titles;
+
+
+# 3 select last names that 
+SELECT
+	last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY last_name;
+
+# 4 select last names that 
+SELECT
+	first_name,
+	last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY first_name, last_name;
+
+# 5 last name contains q but not qu
+SELECT DISTINCT
+	last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
+# 6 last name contains q but not qu, count each
+SELECT DISTINCT
+	last_name,
+	count(last_name)
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+# 7 count how many Irena, Vidya, Maya
+SELECT
+	count(*),
+	gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
+
+
+# 8 duplicates
+SELECT
+	lower(concat(substr(first_name, 1, 1), substr(last_name, 1, 4), '_', substr(birth_date, 6, 2), substr(birth_date, 3, 2))) AS username,
+	COUNT(*)
+FROM employees
+GROUP BY username
+HAVING count(username) > 1;
+
+# BONUS 13251
+
+
+
+
+
+
+
