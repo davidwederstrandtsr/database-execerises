749e3ad215d990ff684c41a464f069539e1e0210
David Michael Wederstrandt Sr <david.wederstrandtsr@gmail.com>
2020-02-11 11:04:50 -0600

    ex 3.6

---
+++ resource/02-10-2020-SQL-Order_by.sql
@@ -0,0 +1,21 @@
+USE employees;
+
+SELECT *
+FROM employees
+ORDER BY emp_no DESC;
+
+
+SELECT *
+FROM employees
+ORDER BY first_name; # order by first name
+
+SELECT *
+FROM employees
+ORDER BY first_name, last_name; # order by first name than last name
+
+SELECT *
+FROM employees
+ORDER BY first_name, last_name DESC; # order by first name, than last name but last name in descending order
+
+
+
