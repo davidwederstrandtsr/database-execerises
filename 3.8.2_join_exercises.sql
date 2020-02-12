aa41566d5beaaf061a52fc0a4856981856b69917
David Michael Wederstrandt Sr <david.wederstrandtsr@gmail.com>
2020-02-12 14:34:00 -0600

    update join

--- 3.8.2_join_exercises.sql
+++ 3.8.2_join_exercises.sql
@@ -1,3 +1,4 @@
+USE join_examples;
 # 1
 SELECT * FROM users;
 SELECT * FROM roles;
@@ -6,7 +7,7 @@ SELECT * FROM roles;
 # JOIN
 SELECT *
 FROM users
-JOIN roles ON roles.id = users.id;
+JOIN roles ON users.id = roles.id;
 
 # LEFT JOIN 
 SELECT *
@@ -20,13 +21,19 @@ RIGHT JOIN roles ON roles.id = users.id;
 
 # 3 COUNT
 SELECT 
-	roles.name, 
-	count(roles.name)
-FROM roles
-RIGHT JOIN users;
+	r.name, 
+	COUNT(*) AS count_all,
+	COUNT(u.id) AS count_user # will only count if not null 
+FROM roles r
+LEFT JOIN users u ON u.role_id = r.id
+GROUP BY r.name;
 
 
 # EMPLOYEES DB
+
+USE employees;
+
+
 
 # 2
 SELECT 
@@ -38,16 +45,39 @@ JOIN departments ON departments.dept_no = dept_manager.dept_no
 WHERE dept_manager.to_date = '9999-01-01'
 ORDER BY departments.dept_name;
 
+# 2 revisited
+SELECT 
+	dept_name AS "Department Name",
+	CONCAT(first_name, ' ', last_name) AS "Department Manager"
+FROM departments d
+JOIN dept_manager dm ON dm.dept_no = d.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no
+WHERE dm.to_date > NOW();
+
+
 
 # 3
 SELECT 
-	departments.dept_name AS "Department Name",
+	departments.dept_name AS "Department NAME",
 	CONCAT(employees.first_name, ' ', employees.last_name) AS "Department Manager"
 FROM employees
 JOIN dept_manager ON dept_manager.emp_no = employees.emp_no
 JOIN departments ON departments.dept_no = dept_manager.dept_no
 WHERE dept_manager.to_date = '9999-01-01' AND employees.gender LIKE 'F'
 ORDER BY departments.dept_name;
+
+# 3 revisited
+SELECT 
+	d.dept_name AS `Department Name`,
+	CONCAT(first_name, ' ', last_name) AS `Manager Name`
+FROM employees e
+JOIN dept_manager dm ON dm.emp_no = e.emp_no
+JOIN departments d ON d.dept_no = dm.dept_no
+WHERE dm.to_date > curdate()
+AND gender = 'F'
+ORDER BY `Department Name`;
+
+
 
 # 4
 SELECT
@@ -60,10 +90,24 @@ WHERE de.dept_no = "d009" AND de.to_date = '9999-01-01' AND t.to_date = "9999-01
 GROUP BY Title
 ORDER BY Title;
 
+# 4 revisited
+SELECT 
+	title AS "Title",
+	COUNT(*) AS "Count"
+FROM departments d
+JOIN dept_emp de ON  de.dept_no = d.dept_no
+JOIN titles t ON t.emp_no = de.emp_no
+WHERE t.to_date > curdate()
+	AND de.to_date > CURDATE() 
+	AND dept_name = "Customer Service"
+GROUP BY title;
+
+
+
 # 5
 SELECT 
-	d.dept_name AS "Department Name",
-	CONCAT(e.first_name, ' ', e.last_name) AS "Name",
+	d.dept_name AS "Department NAME",
+	CONCAT(e.first_name, ' ', e.last_name) AS "NAME",
 	s.salary AS "Salary"
 FROM employees AS e
 JOIN dept_manager AS dm ON dm.emp_no = e.emp_no
@@ -72,16 +116,40 @@ JOIN salaries AS s ON s.emp_no = dm.emp_no
 WHERE dm.to_date = '9999-01-01' AND s.to_date = "9999-01-01"
 ORDER BY d.dept_name;
 
+# 5 revisited
+SELECT 
+	dept_name AS `Department Name`,
+	CONCAT(first_name, ' ', last_name) AS `Manager Name`,
+	salary AS `Salary`
+FROM salaries s
+JOIN dept_manager dm ON dm.emp_no = s.emp_no
+JOIN employees e ON e.emp_no = dm.emp_no
+JOIN departments d ON d.dept_no = dm.dept_no
+WHERE s.to_date > CURDATE()
+	AND dm.to_date > CURDATE()
+	ORDER BY `Department Name`;
+
 
 # 6
 SELECT
 	d.dept_no,
 	d.dept_name,
-	COUNT(d.dept_no)
+	COUNT(d.dept_no) AS `Number of employees`
 FROM departments d
 JOIN dept_emp de USING (dept_no)
 WHERE de.to_date = '9999-01-01'
 GROUP BY d.dept_no;
+
+# 6 revisited
+SELECT 
+	dept_no,
+	dept_name,
+	COUNT(*) AS `num_employees`
+FROM departments d
+JOIN dept_emp de USING (dept_no)
+WHERE to_date > NOW()
+GROUP BY dept_no;
+
 
 
 # 7 
@@ -96,6 +164,11 @@ GROUP BY d.dept_name
 ORDER BY average_salary DESC
 LIMIT 1;
 
+# 7 revisited
+
+
+
+
 # 8 
 SELECT
 	first_name,
@@ -104,9 +177,13 @@ FROM employees e
 JOIN dept_emp de ON de.emp_no = e.emp_no
 JOIN salaries s ON s.emp_no = de.emp_no
 JOIN departments d ON d.dept_no = de.dept_no 
-WHERE s.to_date = '9999-01-01' AND de.to_date = "9999-01-01" AND d.dept_no = 'd001'
+WHERE s.to_date = '9999-01-01' 
+	AND de.to_date = "9999-01-01" 
+	AND d.dept_no = 'd001'
 ORDER BY salary DESC
 LIMIT 1;
+
+
 
 # 9
 SELECT
@@ -126,9 +203,9 @@ LIMIT 1;
 
 # 10 bonus
 SELECT
-	CONCAT(e.first_name, ' ', e.last_name) AS "Employee Name",
-	d.dept_name AS "Department Name",
-	CONCAT(dmn.first_name, ' ', dmn.last_name) AS "Manager Name"
+	CONCAT(e.first_name, ' ', e.last_name) AS "Employee NAME",
+	d.dept_name AS "Department NAME",
+	CONCAT(dmn.first_name, ' ', dmn.last_name) AS "Manager NAME"
 FROM employees e
 JOIN dept_emp de ON de.emp_no = e.emp_no
 JOIN departments d ON d.dept_no = de.dept_no
@@ -140,8 +217,8 @@ ORDER BY d.dept_name, e.first_name, e.last_name;
 
 # 11
 SELECT
-	CONCAT(e.first_name, ' ', e.last_name) AS "Employee Name",
-	d.dept_name AS "Department Name",
+	CONCAT(e.first_name, ' ', e.last_name) AS "Employee NAME",
+	d.dept_name AS "Department NAME",
 	salary
 FROM employees e
 JOIN dept_emp de ON de.emp_no = e.emp_no
