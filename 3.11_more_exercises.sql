399d698bc84e64178c78d5f4a214bde5aba70b74
David Michael Wederstrandt Sr <david.wederstrandtsr@gmail.com>
2020-02-15 22:51:36 -0600

    updating all

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -1,7 +1,24 @@
 # 1 How much do the current managers of each department get paid, relative to the average salary for the department? Is there any department where the department manager gets paid less than the average salary?
 DROP TABLE manager_pay;
+DROP TABLE dept_avg_pay;
 
-CREATE TEMPORARY TABLE manager_pay AS 
+
+CREATE TEMPORARY TABLE dept_avg_pay AS
+SELECT 
+	dept_name,
+	AVG(salary) AS `dept_avg_salary`
+FROM employees.salaries es
+JOIN employees.dept_emp ede USING(emp_no)
+JOIN employees.departments de ON de.dept_no = ede.dept_no
+WHERE ede.to_date > NOW()
+	AND es.to_date > NOW()
+GROUP BY dept_name;
+
+SELECT *
+FROM dept_avg_pay;
+
+
+CREATE TABLE manager_pay AS 
 SELECT
 	CONCAT(first_name, ' ', last_name) AS `Manager`,
 	dept_name,
@@ -16,15 +33,12 @@ WHERE emp_no IN (
 	WHERE to_date > CURDATE())
 AND salaries.to_date > NOW();
 
+
 SELECT * FROM manager_pay;
 
-SELECT 
-	dept_name,
-	AVG(salary) AS `dept_avg_salary`
-FROM employees.salaries
-JOIN employees.dept_emp USING(emp_no)
-JOIN employees.departments ON 
-GROUP BY dept_name
-;
+SELECT *
+FROM manager_pay
+JOIN dept_avg_pay dap USING ()
+
 
 
